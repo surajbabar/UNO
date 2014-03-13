e1d1fcd44484c509794560cc8afd49fef8ce2947
surajbabar <surajbab@thoughtworks.com>
2014-03-13 14:30:26 +0530

    deck returns new deck on it's creation

--- js/model/deck.js
+++ js/model/deck.js
@@ -1,36 +1,39 @@
 var deck = {};
-var card = require('card.js').card;
+var card = require('./card.js').card;
 
-deck.create =function(numberOfPacks){
-	deck.cards=card.getCardPack(numberOfPacks);
+deck.create = function(numberOfPacks){
+	deck.cards = card.getCardPack(numberOfPacks);
+	return deck;
 };
 
-deck.shuffle =function(){
+deck.shuffle = function(){
 	deck.cards.sort(function() { return 0.5 - Math.random()});
 };
 
-deck.draw =function(){
-	deck.cards.splice(0,1); //removes first element
+deck.draw = function(){
+	deck.cards.splice(0,1); //removes first card
 };
 
-deck.add =function(card){
+deck.add = function(card){
 	deck.cards.push(card);
 };
 
-deck.lookAtLast =function(){
+deck.lookAtLast = function(){
 	return deck.cards[deck.cards.length-1];
 };
 
-deck.isEmpty=function(){
+deck.isEmpty = function(){
 	return deck.cards.length==0;
 };
 
-deck.addAll =function(newCards[]){
+deck.addAll = function(newCards){
 	newCards.forEach(function(card){
 	deck.cards.push(card);
 	});
 };
 
 deck.drawAllButLast =function(){
-		return deck.cards.splice(1,deck.cards.length-1);
+	return deck.cards.splice(1,deck.cards.length-1);
 };
+
+exports.deck=deck;
