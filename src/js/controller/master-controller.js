ea8618c1cfc7e59ee05d1b5722847e6b63357d6f
MdDubey <md@whatever.com>
2014-03-14 20:32:55 +0530

    Simple client and server created

--- src/js/controller/master-controller.js
+++ src/js/controller/master-controller.js
@@ -1,3 +1,23 @@
-var uno = angular.module('uno' , []);
+var uno = angular.module('uno', []);
+var net = require('net');
+var sockets = [];
 
 
+uno.controller('startCtrl', function ($scope) {
+    $scope.createGame = function () {
+        var server = net.createServer();
+        server.listen(8080);
+
+        server.on('connection', function (socket) {
+            if (sockets.length == $scope.noOfPlayers)
+                return;
+            sockets.push(socket);
+            if (sockets.length == $scope.noOfPlayers) {
+                sockets.forEach(function (clientSocket) {
+                    clientSocket.write('start');
+                });
+                console.log('all connections got');
+            }
+        });
+    }
+});
