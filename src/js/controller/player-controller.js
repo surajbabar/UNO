a4d99994a30427e3bd675bdab52d7616c58c1112
surajbabar <surajbab@thoughtworks.com>
2014-03-17 17:08:41 +0530

    player screen is ready with it's placeholders

--- src/js/controller/player-controller.js
+++ src/js/controller/player-controller.js
@@ -1,13 +1,27 @@
-var uno = angular.module('uno', []);
-var net = require('net');
-
-uno.controller('loadGame',function($scope){
-    $scope.masterName = "127.0.0.1";
+var uno = angular.module('player-screen',[]);
+//var net = require('net');
+uno.controller('initial',function($scope){
     $scope.playerName = "me";
 
-})
-exports.start =function(master,snapshot){
+});
 
-    console.log("suraj");
-    window.open("../../view/player-screen.html");
-}
+uno.controller('otherPlayers',function($scope){
+    $scope.players=[{name:"suraj",noOfCards:5},{name:"mritunjay",noOfCards:5},{name:"suraj",noOfCards:5},{name:"mritunjay",noOfCards:5}];
+    $scope.activityLog="sdgsxdfghjkjhgf";
+    $scope.myCards=[{color:"red",sign:"4"},{color:"red",sign:"4"},{color:"red",sign:"4"},{color:"red",sign:"4"},{color:"red",sign:"4"}];
+    var update = function(snapshot){
+        $scope.activityLog=$scope.activityLog+'\n'+snapshot.currentTurnLog;
+        $scope.openCard=snapshot.openCard;
+        $scope.hint=snapshot.hint;
+
+    }
+    for(var i=0;i<15;i++){
+        update({currentTurnLog:"sadf",openCard:{sign:'32'},hint:"play a red or 3"});
+    }
+
+//    console.log($scope);
+});
+//exports.start =function(master,snapshot,playerName){
+//
+//    console.log("suraj");
+//}
