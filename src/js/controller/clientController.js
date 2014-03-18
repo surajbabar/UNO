27f9689586a9450bf4941b4f7e85ea255b126f4b
MdDubey <md@whatever.com>
2014-03-18 18:44:31 +0530

    window interactions are working properly,passing snapshot from master to player is done

--- src/js/controller/clientController.js
+++ src/js/controller/clientController.js
@@ -1,13 +1,20 @@
 var uno = angular.module('clientController', []);
 var net = require('net');
+var gui = require('nw.gui');
 
 uno.service('playerService', function () {
-    this.playerData = {data: ''};
+    this.playerData = {data: '', socket: ''};
     this.setData = function (value) {
         this.playerData.data = value;
     }
     this.getData = function () {
         return this.playerData.data;
+    }
+    this.setSocket = function (socket) {
+        this.playerData.socket = socket;
+    }
+    this.getSocket = function () {
+        return this.playerData.socket;
     }
 })
 
@@ -16,6 +23,7 @@ uno.controller('joinCtrl', function ($scope, $location, $route, playerService) {
     $scope.playerName = "me";
     var client;
     $scope.joinGame = function () {
+        gui.Window.get().hide();
         client = net.connect({port: 8080, host: $scope.masterName}, function () {
             var introduction = {type: 'introduction', playerName: $scope.playerName};
             client.write(JSON.stringify(introduction));
@@ -23,35 +31,26 @@ uno.controller('joinCtrl', function ($scope, $location, $route, playerService) {
 
         client.on('data', function (data) {
             playerService.setData(data);
+            playerService.setSocket(client);
             $location.url('player');
             $route.reload();
+            gui.Window.get().show();
         });
     };
 });
 
-uno.controller('playerCtrl', function ($scope, playerService) {
-    console.log(playerService.getData().toString());
-    $scope.players = [
-        {name: "suraj", noOfCards: 5},
-        {name: "mritunjay", noOfCards: 5},
-        {name: "suraj", noOfCards: 5},
-        {name: "mritunjay", noOfCards: 5}
-    ];
-    $scope.activityLog = "sdgsxdfghjkjhgf";
-    $scope.myCards = [
-        {color: "red", sign: "4"},
-        {color: "red", sign: "4"},
-        {color: "red", sign: "4"},
-        {color: "red", sign: "4"},
-        {color: "red", sign: "4"}
-    ];
-    var update = function (snapshot) {
-        $scope.activityLog = $scope.activityLog + '\n' + snapshot.currentTurnLog;
-        $scope.openCard = snapshot.openCard;
-        $scope.hint = snapshot.hint;
+var update = function (snapshot, $scope) {
+    $scope.activityLog = "";
+    $scope.hint = "";
+    $scope.players = snapshot.playerSummaries;
+    $scope.myCards = snapshot.myCards;
+    $scope.activityLog = $scope.activityLog + '\n' + snapshot.currentTurnLog;
+    $scope.openCard = snapshot.openCard;
+    $scope.hint = snapshot.hint;
+}
 
-    }
-    for (var i = 0; i < 15; i++) {
-        update({currentTurnLog: "sadf", openCard: {sign: '32'}, hint: "play a red or 3"});
-    }
+uno.controller('playerCtrl', function ($scope, playerService) {
+    var snapshot = JSON.parse(playerService.getData());
+    console.log(snapshot);
+    update(snapshot, $scope);
 })
