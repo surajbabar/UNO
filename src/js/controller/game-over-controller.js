cfba330797c16021397e042d34dfd753d62c39ce
MdDubey <md@whatever.com>
2014-04-01 18:42:30 +0530

    controllers seperated according to their work

---
+++ src/js/controller/game-over-controller.js
@@ -0,0 +1,15 @@
+var uno = angular.module('clientController');
+
+uno.controller('gameOverCtrl', function ($scope, playerService) {
+    var result = playerService.getGameResult();
+    $scope.players = result.playerResults;
+
+    $scope.$watch("players.cards", function () {
+        $scope.numberOfCardsLeft = function (player) {
+            return {
+                width: (player.cards.length * 45) + 'px'
+            };
+        };
+    });
+
+})
