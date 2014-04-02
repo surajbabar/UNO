4e334565eb93caaa327d5395813f70cc1abb1b14
surajbabar <surajbab@thoughtworks.com>
2014-04-02 12:43:34 +0530

    waiting for user screen implemented

--- src/js/controller/join-controller.js
+++ src/js/controller/join-controller.js
@@ -1,16 +1,20 @@
 angular.module('clientController').controller('joinCtrl', function ($scope, $location, $route, playerService) {
     $scope.masterName = "127.0.0.1";
     $scope.playerName = "me";
+    $scope.hasEveryoneJoined = false;
+
     var client;
     var firstTimeData = true;
     $scope.joinGame = function () {
         client = net.connect({port: 8080, host: $scope.masterName}, function () {
             var introduction = {type: 'introduction', playerName: $scope.playerName};
             client.write(JSON.stringify(introduction));
-            gui.Window.get().hide();
+            $scope.hasEveryoneJoined = true;
+            $scope.$apply();
         });
 
         function switchToPlayerScreen(data) {
+            gui.Window.get().hide();
             playerService.changeData(data);
             playerService.setSocket(client);
             $location.url('player');
