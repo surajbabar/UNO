584e63f45713ac296a070c015bbfb0ab5d35259c
MdDubey <md@whatever.com>
2014-04-07 12:11:06 +0530

    Changing code to HTTP compatible

--- src/js/controller/master-controller.js
+++ src/js/controller/master-controller.js
@@ -3,13 +3,16 @@ var gameMaster = require('game-master.js');
 var net = require('net');
 
 
-uno.controller('startCtrl', function ($scope) {
+uno.controller('startCtrl', function ($scope, $http) {
+    $scope.masterName = "me";
     $scope.noOfPacks = 1;
     $scope.noOfPlayers = 1;
-	$scope.gameNotCreated = true;
+    $scope.gameNotCreated = true;
 
-    $scope.createGame = function(){
-        gameMaster.startGameMaster($scope.noOfPacks,$scope.noOfPlayers);
-		$scope.gameNotCreated = false;
+    $scope.createGame = function () {
+        var data = {noOfPacks: $scope.noOfPacks, noOfPlayers: $scope.noOfPlayers,masterName:$scope.masterName};
+        $http({method: 'post', url: 'http://uno-step2013.rhcloud.com/startGame', data: data}).success(function () {
+            $scope.gameNotCreated = false;
+        });
     }
 });
