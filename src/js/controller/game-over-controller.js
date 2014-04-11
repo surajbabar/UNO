8cd74e9013ac049e4792abe9c5221c85ab248df7
tanbirka <kaurTanbir>
2014-04-11 15:03:19 +0530

    result is being displayed in table format having hover

--- src/js/controller/game-over-controller.js
+++ src/js/controller/game-over-controller.js
@@ -1,5 +1,6 @@
 var uno = angular.module('clientController');
 
+
 uno.controller('gameOverCtrl', function ($scope, playerService) {
     var result = playerService.getGameResult();
     $scope.players = result.playerResults;
