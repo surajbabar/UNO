ebd6c2ea743a0d8dd2e5057d6a45a65537f8ed06
MdDubey <md@whatever.com>
2014-04-15 16:54:22 +0530

    now cards in hand will be shown according to color

--- src/js/controller/player-controller.js
+++ src/js/controller/player-controller.js
@@ -16,6 +16,11 @@ var getProperImage = function (card) {
 
 var update = function (snapshot, $scope) {
     $scope.players = checkForUno(snapshot.playerSummaries);
+
+    snapshot.myCards.sort(function(card1,card2){
+        return card1.color > card2.color;
+    });
+    
     $scope.myCards = snapshot.myCards;
     if (snapshot.currentTurnLog != '' && $scope.activityLog[0] != snapshot.currentTurnLog)
         $scope.activityLog.splice(0, 0, snapshot.currentTurnLog);
